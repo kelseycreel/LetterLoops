5c53becea8f5c4b5425b6e887cc76aa365ffedaa
Kelsey Creel <kelseycreel@gmail.com>
2020-02-29 12:12:45 -0600

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a string:");
+            var input = Console.ReadLine().ToLower();
+
+            // repeat each character plus one based on its position
+            var outputString = "";
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                for (var j = 0; j < input.Length; j++)
+                {
+                    if (j == 0)
+                    {
+                        var capitalized = input.ToUpper();
+                        outputString += capitalized[i];
+                    }
+                    else
+                        outputString += input[i];
+                }
+                if (i != input.Length)
+                {
+                    outputString += "-";
+                }
+            }
+            Console.WriteLine(outputString);
+            Console.ReadKey();
+        }
+    }
+}
